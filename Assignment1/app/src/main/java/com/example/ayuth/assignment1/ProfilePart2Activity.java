7f2ef81b32ea6b6eb9f7eb0e732a35daa77792c3
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-09-16 23:54:39 +0700

    Init project

---
+++ Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePart2Activity.java
@@ -0,0 +1,13 @@
+package com.example.ayuth.assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilePart2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_part_2);
+    }
+}
