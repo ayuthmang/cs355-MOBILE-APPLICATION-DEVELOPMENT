4843be7eb8f083cf88be4964e6e0404fa46abaf6
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-09-20 01:53:36 +0700

    Finished with activity profile part 1

--- Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartOneActivity.java
+++ Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartOneActivity.java
@@ -1,9 +1,27 @@
 package com.example.ayuth.assignment1;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ProfilePartOneActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class ProfilePartOneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etName;
+    EditText etLastName;
+    EditText etBirthDate;
+    EditText etEmail;
+    EditText etPhoneNo;
+    Button btnSubmit;
+
+    DatePickerDialog birthdatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +31,43 @@ public class ProfilePartOneActivity extends AppCompatActivity {
     }
 
     public void initInstances() {
+        etName = findViewById(R.id.etFirstName_part_1);
+        etLastName = findViewById(R.id.etLastName_part_1);
+        etBirthDate = findViewById(R.id.etBirthDate);
+        etEmail = findViewById(R.id.etEmail_part_1);
+        etPhoneNo = findViewById(R.id.etPhone_part_1);
+        btnSubmit = findViewById(R.id.btnSubmit);
 
+        btnSubmit.setOnClickListener(this);
+        etBirthDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnSubmit) {
+            Intent profilePartTwo = new Intent(getBaseContext(), ProfilePartTwoActivity.class);
+            profilePartTwo.putExtra("name", etName.getText().toString());
+            profilePartTwo.putExtra("lastname", etLastName.getText().toString());
+            profilePartTwo.putExtra("birthdate", etBirthDate.getText().toString());
+            profilePartTwo.putExtra("email", etEmail.getText().toString());
+            profilePartTwo.putExtra("phone", etPhoneNo.getText().toString());
+            Toast.makeText(this, "kleJLKJLRKG", Toast.LENGTH_SHORT).show();
+            startActivity(profilePartTwo);
+        } else if (v == etBirthDate) {
+            final Calendar cldr = Calendar.getInstance();
+            int day = cldr.get(Calendar.DAY_OF_MONTH);
+            int month = cldr.get(Calendar.MONTH);
+            int year = cldr.get(Calendar.YEAR);
+            // date picker dialog
+            birthdatePicker = new DatePickerDialog(ProfilePartOneActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            etBirthDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        }
+                    }, year, month, day);
+            birthdatePicker.show();
+        }
     }
 }
+
--- Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartTwoActivity.java
+++ Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartTwoActivity.java
@@ -2,8 +2,15 @@ package com.example.ayuth.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ProfilePartTwoActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etLastName;
+    EditText etAge;
+    EditText etEmail;
+    EditText etPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
