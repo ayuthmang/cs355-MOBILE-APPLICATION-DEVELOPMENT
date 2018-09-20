199adc12b875f9e1f091dd7aead1b42a6f1e29b6
Ayuth Mangmesap <ayuth.mang@gmail.com>
2018-09-20 13:00:36 +0700

    Accept value from first activity and put in 2nd form activity

--- Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartTwoActivity.java
+++ Assignment1/app/src/main/java/com/example/ayuth/assignment1/ProfilePartTwoActivity.java
@@ -3,6 +3,10 @@ package com.example.ayuth.assignment1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class ProfilePartTwoActivity extends AppCompatActivity {
 
@@ -11,11 +15,13 @@ public class ProfilePartTwoActivity extends AppCompatActivity {
     EditText etAge;
     EditText etEmail;
     EditText etPhone;
+    ImageView imgProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_part_two);
+        initInstances();
     }
 
     public void initInstances() {
@@ -24,5 +30,36 @@ public class ProfilePartTwoActivity extends AppCompatActivity {
         etAge = findViewById(R.id.etAge);
         etEmail = findViewById(R.id.etEmail_part_2);
         etPhone = findViewById(R.id.etPhone_part_2);
+        imgProfile = findViewById(R.id.imgProfile);
+
+        etName.setText(getIntent().getStringExtra("name"));
+        etLastName.setText(getIntent().getStringExtra("lastname"));
+        etAge.setText(getAgeByBirthdate(getIntent().getStringExtra("birthdate")));
+        etEmail.setText(getIntent().getStringExtra("email"));
+        etPhone.setText(getIntent().getStringExtra("phone"));
+    }
+
+
+    private int setImageByAge(int age) {
+        return 0;
+    }
+
+    private String getAgeByBirthdate(String DOB) {
+        Calendar dob = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+
+        String[] birthdate = DOB.split("/");
+        dob.set(Integer.parseInt(birthdate[2]), Integer.parseInt(birthdate[1]), Integer.parseInt(birthdate[0]));
+
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+
+        Integer ageInt = new Integer(age);
+        String ageS = ageInt.toString();
+
+        return ageS;
     }
 }
